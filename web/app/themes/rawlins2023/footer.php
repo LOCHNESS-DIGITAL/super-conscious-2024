00f57a2f9362548258289eeaf862683123fe0d62
Chris Hodges <chodges@forumone.com>
2024-10-03 11:31:37 -0400

    footer updates

--- web/app/themes/rawlins2023/footer.php
+++ web/app/themes/rawlins2023/footer.php
@@ -2,7 +2,6 @@
 $global_options = get_field('global_options', 'option');
 $emails = $global_options['contact_email_addresses'];
 ?>
-<?php if ( !post_password_required( $post ) ): ?>
 <footer class="footer">
     <div class="footer__inner l-container">
         <div class="footer__content">
@@ -14,24 +13,23 @@ $emails = $global_options['contact_email_addresses'];
             <div class="footer__since">Since 2016.</div>
             <div class="footer__copyright">&copy;<?php echo date('Y'); ?> Super Conscious&reg;</div>
         </div>
-        <div class="footer__skull"><a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_stylesheet_directory_uri() . '/images/website-wrkn-004.png'; ?>" alt="" /></a></div>
+        <div class="footer__skull"><a target="_blank" href="https://open.spotify.com/user/31rsi3owjmpkkppsgdilnfsvwvay"><img src="<?php echo get_stylesheet_directory_uri() . '/images/icon-playlist.svg'; ?>" alt="" /></a></div>
     </div>
 </footer>
 
-</div>
-    <?php if ( is_front_page() ) : ?>
-        <section id="modal" class="modal">
-            <div class="modal__inner">
-                <div class="modal__close"><span><?php echo file_get_contents(get_stylesheet_directory() . '/images/icon__modal--close.svg'); ?></span></div>
-                <nav class="modal__nav l-container">
-                    <span class="modal__nav__item modal__nav__item--prev"><?php echo file_get_contents(get_stylesheet_directory() . '/images/icon__modal--prev.svg'); ?></span>
-                    <span class="modal__nav__item modal__nav__item--next"><?php echo file_get_contents(get_stylesheet_directory() . '/images/icon__modal--next.svg'); ?></span>
-                </nav>
-                <div class="modal__images">
-                </div>
+</div> <!--site-container-->
+<?php if ( is_front_page() ) : ?>
+    <section id="modal" class="modal">
+        <div class="modal__inner">
+            <div class="modal__close"><span><?php echo file_get_contents(get_stylesheet_directory() . '/images/icon__modal--close.svg'); ?></span></div>
+            <nav class="modal__nav l-container">
+                <span class="modal__nav__item modal__nav__item--prev"><?php echo file_get_contents(get_stylesheet_directory() . '/images/icon__modal--prev.svg'); ?></span>
+                <span class="modal__nav__item modal__nav__item--next"><?php echo file_get_contents(get_stylesheet_directory() . '/images/icon__modal--next.svg'); ?></span>
+            </nav>
+            <div class="modal__images">
             </div>
-        </section>
-    <?php endif; ?>
+        </div>
+    </section>
 <?php endif; ?>
 <?php wp_footer(); ?>
 
